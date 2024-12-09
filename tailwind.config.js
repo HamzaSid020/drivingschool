6a55109335c2ed129c135aca6f9ecc353ba3b476
Aeroer Mak <58320066+AeroerMak@users.noreply.github.com>
2024-12-09 14:32:13 +0800

    Develop 1st Page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./src/**/*.php",
+    ],
+    theme: {
+
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+
+        fontFamily: {
+            'body': ['Be Vietnam Pro', 'sans-serif'],
+        },      
+    },
+}
